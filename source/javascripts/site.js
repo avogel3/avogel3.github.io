3f6180efd9ed77171f56e548ec00ce59d4e687de
Andrew Vogel <andrewpvogel@gmail.com>
2020-05-03 22:08:00 -0400

    Convert to erb templates

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,11 @@
 // This is where it all goes :)
+window.onload = function() {
+  setCopyrightContent()
+}
+
+
+function setCopyrightContent() {
+  var currentYear = new Date().getFullYear();
+  var copyrightElement = document.getElementById('copyright');
+  copyrightElement.textContent = "© 2018-" + currentYear + " Andrew Vogel"
+}
