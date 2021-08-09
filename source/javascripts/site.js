a902ff1a7b1a2ef60d9264301a3838ee19661915
Andrew Vogel <andrewpvogel@gmail.com>
2021-08-09 13:06:45 -0400

    update copright year script/markup

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,11 +1,11 @@
 // This is where it all goes :)
 window.onload = function() {
-  setCopyrightContent()
+  setCopyrightContent();
 }
 
 
 function setCopyrightContent() {
   var currentYear = new Date().getFullYear();
-  var copyrightElement = document.getElementById('copyright');
-  copyrightElement.textContent = "© 2018-" + currentYear + " Andrew Vogel"
+  var copyrightElement = document.getElementById('copyrightYear');
+  copyrightElement.textContent = currentYear;
 }
