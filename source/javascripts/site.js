d52aa4a1e557d437939b212832d01b7743a1f951
Andrew Vogel <andrewpvogel@gmail.com>
2021-12-16 17:34:35 -0500

    working hamburger menu

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,10 +1,23 @@
 // This is where it all goes :)
 window.onload = function() {
   setCopyrightContent();
+
+  const navToggle = document.getElementById("navToggle");
+  navToggle.addEventListener('click', toggleNav)
 }
 
 function setCopyrightContent() {
   var currentYear = new Date().getFullYear();
   var copyrightElement = document.getElementById('copyrightYear');
   copyrightElement.textContent = currentYear;
+}
+
+function toggleNav() {
+  var navLinks = document.getElementById("navLinks");
+  if (navLinks.style.display === "block") {
+    navLinks.style.display = "none";
+    return;
+  }
+
+  navLinks.style.display = "block";
 }
