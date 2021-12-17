5095c21a4c5816b58c633165a8aebf9cd2b5aaa2
Andrew Vogel <andrewpvogel@gmail.com>
2021-12-17 14:18:48 -0500

    Site updated to bafa589

--- javascripts/site.js
+++ javascripts/site.js
@@ -1,6 +1,18 @@
 // This is where it all goes :)
 window.onload = function() {
   setCopyrightContent();
+
+  const hamburger = document.querySelector(".hamburger");
+  const navMenu = document.querySelector("nav > ul");
+  hamburger.addEventListener("click", mobileMenu);
+  function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+  }
+	function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+	}
 }
 
 function setCopyrightContent() {
@@ -8,3 +20,4 @@ function setCopyrightContent() {
   var copyrightElement = document.getElementById('copyrightYear');
   copyrightElement.textContent = currentYear;
 }
+
